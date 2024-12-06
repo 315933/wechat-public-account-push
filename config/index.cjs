90fbca2e167fea5df455c0129d541ec5895ed276
315933 <17676504725@163.com>
2024-12-07 01:40:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'bb',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqWn06uuX8UukJTPc-DilnsVZy5c	',
+      id: 'oqWn06uuX8UukJTPc-DilnsVZy5c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1ioAt-eE9iwnFs6BN2r9lrIz6yBrwOyWhoJjQBG_jX4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
